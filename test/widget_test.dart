2f63b3ca0279327ad3856847dc8e2e27e9260993
Serhii Pimenov <serhii@pimenov.com.ua>
2026-09-13 10:00:51 +0300

    rename package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,4 @@
-import 'package:accu_check/app.dart';
+import 'package:accu_viewer/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
